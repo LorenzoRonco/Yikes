e23ba92681573d4f586cc0502bf94db96508213f
Lorenzo Ronco <115119988+LorenzoRonco@users.noreply.github.com>
2025-06-02 23:45:39 +0200

    first draft index.mjs

--- server/index.mjs
+++ server/index.mjs
@@ -1,9 +1,61 @@
 // imports
 import express from 'express';
+import morgan from 'morgan';
+import {check, validationResult} from 'express-validator';
+import {getUser} from './dao.mjs';
+import cors from 'cors';
+
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+import session from 'express-session';
 
 // init express
 const app = new express();
 const port = 3001;
+
+// middleware
+app.use(express.json());
+app.use(morgan('dev'));
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  optionsSuccessState: 200,
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+
+passport.use(new LocalStrategy(async function verify(username, password, cb) {
+  const user = await getUser(username, password);
+  if(!user)
+    return cb(null, false, 'Incorrect username or password.');
+    
+  return cb(null, user);
+}));
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function (user, cb) {
+  return cb(null, user);
+});
+
+const isLoggedIn = (req, res, next) => {
+  if(req.isAuthenticated()) {
+    return next();
+  }
+  return res.status(401).json({error: 'Not authorized'});
+}
+
+app.use(session({
+  secret: "shhhhh... it's a secret!",
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.authenticate('session'));
+
+/** */ ROUTES **/
 
 // activate the server
 app.listen(port, () => {
